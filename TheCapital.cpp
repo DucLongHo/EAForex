85d24777e57580d92d16264091f992174600c85e
LongHD <longquanthinh1@gmail.com>
2026-03-17 11:21:45 +0700

    Fix bug

--- TheCapital.cpp
+++ TheCapital.cpp
@@ -9,7 +9,7 @@ datetime CandleCloseTime; // Biến kiểm tra giá chạy 1p một lần
 // Input parameters
 input double RiskTrade = 15; // Rủi ro long trade (USD)
 input double ProfitBreakEvent = 5; // Lợi nhuận để BE (USD)
-input double TrailingStartProfit = 1.5;  // Mốc lợi nhuận trailing stop
+input double TrailingStartPoint = 500;  // Mốc lợi nhuận trailing stop
 
 //+------------------------------------------------------------------+
 //| Expert initialization function                                   |
@@ -27,7 +27,7 @@ void OnTimer(){
 
     bool isRunningEa = false;
     if(currentCandleCloseTime != CandleCloseTime && 
-        currentCandleCloseTime - currentTime <= 1){
+        currentCandleCloseTime - currentTime <= 2){
         CandleCloseTime = currentCandleCloseTime;
         isRunningEa = true;
 
@@ -59,8 +59,7 @@ void RunningEA(){
     int copied = CopyRates(_Symbol, _Period, 0, 10, rates);
     if(copied <= 0) return;
     
-    TradeNosdCandle(rates);
-    TradeMazubozuCandle(rates);
+    Trading(rates);
 }
 void CloseAllPositions(){
     for(int index = PositionsTotal() - 1; index >= 0 && !IsStopped(); index--){
@@ -74,68 +73,45 @@ void CloseAllPositions(){
     }
 }
 
-void TradeNosdCandle(const MqlRates &rates[]){
+void Trading(const MqlRates &rates[]){
     MqlRates candle = rates[0], secondCandle = rates[1], thirdCandle = rates[2];
-
-    if(candle.close > candle.open){
-        if(secondCandle.close < secondCandle.open  
-            && candle.close > secondCandle.open
-            && candle.high > secondCandle.high
-            && candle.low < secondCandle.low
-            && candle.low < thirdCandle.low
-        ){
-            double entry = SymbolInfoDouble(_Symbol, SYMBOL_ASK);
-            double stopLossDistance = MathAbs(entry - candle.low);
-            double lotSize = GetLotSize(stopLossDistance, candle);
-            double takeProfit = entry + 2 * stopLossDistance;
-
-            if(!Trade.Buy(lotSize, _Symbol, entry, candle.low - 200 * _Point, takeProfit)){
-                Print("Error placing Buy Order: ", Trade.ResultRetcode());
-            }
-        }
-    } else if(candle.close < candle.open){
-        if(secondCandle.close > secondCandle.open 
-            && candle.close < secondCandle.open
-            && candle.low < secondCandle.low
-            && candle.high > secondCandle.high
-            && candle.high > thirdCandle.high
-        ){
-            double entry = SymbolInfoDouble(_Symbol, SYMBOL_BID);
-            double stopLossDistance = MathAbs(entry - candle.high);
-            double lotSize = GetLotSize(stopLossDistance, candle);
-            double takeProfit = entry - 2 * stopLossDistance;
-
-            if(!Trade.Sell(lotSize, _Symbol, entry, candle.high + 200 * _Point, takeProfit)){
-                Print("Error placing Sell Order: ", Trade.ResultRetcode());
-            }
-        }
-    }
-}
-
-void TradeMazubozuCandle(const MqlRates &rates[]){
-    MqlRates candle = rates[0];
+    
     double upperShadow = candle.high - MathMax(candle.open, candle.close);
     double lowerShadow = MathMin(candle.open, candle.close) - candle.low;
 
     if(candle.close > candle.open){
-        if(upperShadow <= 0.1 * (candle.close - candle.open)){
-            double entry = SymbolInfoDouble(_Symbol, SYMBOL_ASK);
-            double stopLossDistance = MathAbs(entry - candle.low);
-            double lotSize = GetLotSize(stopLossDistance, candle);
+        if(secondCandle.close < secondCandle.open  
+            && candle.close > secondCandle.open
+            && candle.open >= secondCandle.close
+            && candle.high > secondCandle.high
+            && candle.low < secondCandle.low
+            && candle.low < thirdCandle.low
+            && candle.close - candle.open > upperShadow
+        ){
+            BUY(candle, true); // NoSD
+        }
 
-            if(!Trade.Buy(lotSize, _Symbol, entry, candle.low - 200 * _Point)){
-                Print("Error placing Buy Order: ", Trade.ResultRetcode());
-            }
+        if(upperShadow <= 0.15 * (candle.close - candle.open)
+            && (candle.high > secondCandle.high || candle.low < secondCandle.low)
+        ){
+            BUY(candle); // Mazubozu
         }
     } else if(candle.close < candle.open){
-        if(lowerShadow <= 0.1 * (candle.open - candle.close)){
-            double entry = SymbolInfoDouble(_Symbol, SYMBOL_BID);
-            double stopLossDistance = MathAbs(entry - candle.high);
-            double lotSize = GetLotSize(stopLossDistance, candle);
+        if(secondCandle.close > secondCandle.open 
+            && candle.close < secondCandle.open
+            && candle.open <= secondCandle.close 
+            && candle.low < secondCandle.low
+            && candle.high > secondCandle.high
+            && candle.high > thirdCandle.high
+            && candle.open - candle.close > lowerShadow
+        ){
+            SELL(candle, true); // NoSD
+        }
 
-            if(!Trade.Sell(lotSize, _Symbol, entry, candle.high + 200 * _Point)){
-                Print("Error placing Sell Order: ", Trade.ResultRetcode());
-            }
+        if(lowerShadow <= 0.15 * (candle.open - candle.close)
+            && (candle.high > secondCandle.high || candle.low < secondCandle.low)
+        ){
+            SELL(candle); // Mazubozu
         }
     }
 }
@@ -145,21 +121,16 @@ double GetLotSize(double stopLossDistance, MqlRates &candle){
     double tickValue = SymbolInfoDouble(_Symbol, SYMBOL_TRADE_TICK_VALUE);
     double tickSize = SymbolInfoDouble(_Symbol, SYMBOL_TRADE_TICK_SIZE);
 
-    // Tính số pips tương ứng với stopLossDistance
     double stopLossPips = stopLossDistance / _Point;
 
-    // Tính giá trị pip
     double pipValue = tickValue / (tickSize / _Point);
    
-    // Tính toán kích thước lô
     double lotSize = RiskTrade / (stopLossPips * pipValue);
    
-    // Đảm bảo rằng kích thước lô nằm trong phạm vi cho phép của sàn giao dịch
     double minLot = SymbolInfoDouble(_Symbol, SYMBOL_VOLUME_MIN);
     double maxLot = SymbolInfoDouble(_Symbol, SYMBOL_VOLUME_MAX);
     lotSize = MathMin(MathMax(lotSize, minLot), maxLot);
    
-    // Làm tròn kích thước lô đến số thập phân phù hợp
     double lotStep = SymbolInfoDouble(_Symbol, SYMBOL_VOLUME_STEP);
     lotSize = MathFloor(lotSize / lotStep) * lotStep;
 
@@ -174,15 +145,16 @@ void ManageBreakEven(){
         if(PositionSelectByTicket(ticket)){
             double entryPrice = PositionGetDouble(POSITION_PRICE_OPEN);
             double takeProfit = PositionGetDouble(POSITION_TP);
+            double currentSL = PositionGetDouble(POSITION_SL);
             double profit = PositionGetDouble(POSITION_PROFIT);
 
             if(profit >= ProfitBreakEvent && takeProfit > 0){
-                if(PositionGetInteger(POSITION_TYPE) == POSITION_TYPE_BUY){
+                if(PositionGetInteger(POSITION_TYPE) == POSITION_TYPE_BUY && currentSL < entryPrice){
                     double newStopLoss = entryPrice + 100 * _Point; // Đặt stop loss về giá entry
                     if(!Trade.PositionModify(ticket, newStopLoss, takeProfit)){
                         Print("Error modifying Buy Order #", ticket, " - Error: ", Trade.ResultComment());
                     }
-                } else if(PositionGetInteger(POSITION_TYPE) == POSITION_TYPE_SELL){
+                } else if(PositionGetInteger(POSITION_TYPE) == POSITION_TYPE_SELL && currentSL > entryPrice){
                     double newStopLoss = entryPrice - 100 * _Point; // Đặt stop loss về giá entry
                     if(!Trade.PositionModify(ticket, newStopLoss, takeProfit)){
                         Print("Error modifying Sell Order #", ticket, " - Error: ", Trade.ResultComment());
@@ -203,20 +175,17 @@ void TrailingByProfitUSD(){
     for(int index = PositionsTotal() - 1; index >= 0; index--){
         ulong ticket = PositionGetTicket(index);
         if(PositionSelectByTicket(ticket)){
-            double profit = PositionGetDouble(POSITION_PROFIT);
             double currentSL = PositionGetDouble(POSITION_SL);
             double priceOpen = PositionGetDouble(POSITION_PRICE_OPEN);
             double takeProfit = PositionGetDouble(POSITION_TP);
             double volume = PositionGetDouble(POSITION_VOLUME);
         
-            double pointsD = (TrailingStartProfit / (volume * tickValue)) * tickSize;
-            double step = NormalizeDouble(pointsD, _Digits);
 
-            if(takeProfit == 0){
+            if(takeProfit < 0.00001){
                 // --- XỬ LÝ LỆNH BUY ---
                 if(PositionGetInteger(POSITION_TYPE) == POSITION_TYPE_BUY){
-                    if(profit >= TrailingStartProfit){
-                        double newSL = NormalizeDouble(last_tick.bid - step, _Digits);
+                    if(last_tick.bid - priceOpen >= TrailingStartPoint * _Point){
+                        double newSL = NormalizeDouble(last_tick.bid - TrailingStartPoint * _Point, _Digits);
                             
                         if(currentSL < priceOpen || currentSL == 0){
                             Trade.PositionModify(ticket, priceOpen, 0);
@@ -228,8 +197,8 @@ void TrailingByProfitUSD(){
 
                 // --- XỬ LÝ LỆNH SELL ---
                 if(PositionGetInteger(POSITION_TYPE) == POSITION_TYPE_SELL){
-                    if(profit >= TrailingStartProfit){
-                        double newSL = NormalizeDouble(last_tick.ask + step, _Digits);
+                    if(priceOpen - last_tick.ask >= TrailingStartPoint * _Point){
+                        double newSL = NormalizeDouble(last_tick.ask + TrailingStartPoint * _Point, _Digits);
                         
                         if(currentSL > priceOpen || currentSL == 0){
                             Trade.PositionModify(ticket, priceOpen, 0);
@@ -244,4 +213,38 @@ void TrailingByProfitUSD(){
 
     }
     
+}
+
+void BUY(MqlRates &candle, bool hasTakeProfit = false){
+    double entry = SymbolInfoDouble(_Symbol, SYMBOL_ASK);
+    double sl = candle.low - 200 * _Point;
+    double lotSize = GetLotSize(MathAbs(entry - sl), MqlRates());
+
+    if(hasTakeProfit){
+        double takeProfit = entry + 2 * MathAbs(entry - sl);
+        if(!Trade.Buy(lotSize, _Symbol, entry, sl, takeProfit)){
+            Print("Error placing Buy Order: ", Trade.ResultRetcode());
+        }
+    } else {
+        if(!Trade.Buy(lotSize, _Symbol, entry, sl)){
+            Print("Error placing Buy Order: ", Trade.ResultRetcode());
+        }
+    }
+}
+
+void SELL(MqlRates &candle, bool hasTakeProfit = false){
+    double entry = SymbolInfoDouble(_Symbol, SYMBOL_BID);
+    double sl = candle.high + 200 * _Point;
+    double lotSize = GetLotSize(MathAbs(entry - sl), MqlRates());
+
+    if(hasTakeProfit){
+        double takeProfit = entry + 2 * MathAbs(entry - sl);
+        if(!Trade.Sell(lotSize, _Symbol, entry, sl, takeProfit)){
+            Print("Error placing Sell Order: ", Trade.ResultRetcode());
+        }
+    } else {
+        if(!Trade.Sell(lotSize, _Symbol, entry, sl)){
+            Print("Error placing Sell Order: ", Trade.ResultRetcode());
+        }
+    }
 }
